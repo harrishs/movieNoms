fb0530066aa5102ee443ea808e76ec881bff5d01
Harrish Suhumar <harrish.s@hotmail.ca>
2021-01-09 18:24:54 -0500

    add backdrop for sidebar, potentially update nominations to use id over title

--- src/Components/Nominations/NominationContainer.js
+++ src/Components/Nominations/NominationContainer.js
@@ -3,6 +3,7 @@ import React, {useState} from "react";
 import classes from "./NominationContainer.module.css"
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import Backdrop from "../Backdrop/Backdrop";
 
 const NominationHolder = props => {
     const [toggle, setToggle] = useState(false);
@@ -19,6 +20,8 @@ const NominationHolder = props => {
     }
 
     return (
+        <>
+        <Backdrop clicked={()=>setToggle(false)} show={toggle}/>
         <div className={toggleClasses.join(" ")}>
             {toggleIcon}
             <h1 className={classes.Count}>Number of Nominations: {props.count}</h1>
@@ -26,6 +29,7 @@ const NominationHolder = props => {
             {props.children}
             </div>
         </div>
+        </>
     )
 }
 
